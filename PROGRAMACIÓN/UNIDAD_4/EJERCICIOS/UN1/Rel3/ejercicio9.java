f92851c8cfc0a97a2c180ec0b2a493c0ed99aed4
pelegrinalopezsem <pelegrinalopezsem001.alu@stemgranada.com>
2026-01-09 10:55:57 +0100

    solucion del 9

--- PROGRAMACIÓN/UNIDAD_4/EJERCICIOS/UN1/Rel3/ejercicio9.java
+++ PROGRAMACIÓN/UNIDAD_4/EJERCICIOS/UN1/Rel3/ejercicio9.java
@@ -5,17 +5,17 @@
 
 // //Separamos los numeros
 // let primeracifra = Number(numero.charAt(0))
-// let segundacifra = Number(numero.charAt(1))
+// let segundaCifra = Number(numero.charAt(1))
 
 // //Creamos una condicion
 // if (numero >= 10 && numero <= 99) {
-//         let suma = (primeracifra + segundacifra)
+//         let suma = (primeracifra + segundaCifra)
 //     console.log ("La suma es " + suma)
 // }
 
 // //Mostramos en consola los dos digitos
 // console.log ("La primera cifra es " + primeracifra)
-// console.log ("La segunda cifra es " + segundacifra)
+// console.log ("La segunda cifra es " + segundaCifra)
 
 
 package PROGRAMACIÓN.UNIDAD_4.EJERCICIOS.UN1.Rel3;
@@ -25,29 +25,21 @@ import java.util.Scanner;
 public class ejercicio9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String numero;
-        final int MINIMO = 10;
-        final int MAXIMO = 99;
+        int numero;
         int suma;
-
+        int primeraCifra;
+        int segundaCifra;
 // Pedimos al usuario el numero
         System.out.print("Ingresa el numero: ");
-        numero = scanner.next();
+        numero = scanner.nextInt();
 
 //Vamos a separar los digitos
-        char primeraCifra = numero.charAt(0);
-        char segundaCifra = numero.charAt(1);
-    
+    primeraCifra = numero / 10;
+    segundaCifra = numero % 10;
+    suma = primeraCifra + segundaCifra;
+    System.out.println("La suma de " + primeraCifra + " + " + segundaCifra + " es: " + suma);
     scanner.close();
 
-//Creamos la condicion
-        if (numero >= MINIMO && numero <= MAXIMO){
-            suma = primeraCifra + segundaCifra;
-            System.out.println("La suma de " + primeraCifra + " + " + segundaCifra + " es: " + suma);
-        } else {
-            System.out.println("El numero es o mas chico que 10 o mas grande que 99");
-        }
     }
 }
-
 //Preguntar al profesro como se podria hacer con el chaAt
