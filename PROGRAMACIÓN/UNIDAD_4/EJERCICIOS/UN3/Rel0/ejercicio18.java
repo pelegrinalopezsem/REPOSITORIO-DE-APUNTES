0e96de80be22d0e868dc038460cb21710ff65cf4
pelegrinalopezsem <pelegrinalopezsem001.alu@stemgranada.com>
2026-01-15 09:48:49 +0100

    que pasa

--- PROGRAMACIÓN/UNIDAD_4/EJERCICIOS/UN3/Rel0/ejercicio18.java
+++ PROGRAMACIÓN/UNIDAD_4/EJERCICIOS/UN3/Rel0/ejercicio18.java
@@ -1,15 +1,21 @@
 package PROGRAMACIÓN.UNIDAD_4.EJERCICIOS.UN3.Rel0;
 
 public class ejercicio18 {
-    public static int contarVocalesEnCadena (String cadenaTexto){
+    public static String contarVocalesEnCadena(String cadenaTexto) {
         cadenaTexto = cadenaTexto.toLowerCase();
         int contadorTotalVocales = 0;
-        for (int i = 0; i <= cadenaTexto.length(); i++){
+        for (int i = 0; i < cadenaTexto.length(); i++) {
             char letraCadena = cadenaTexto.charAt(i);
-            if (letraCadena == "A" || letraCadena == "E" || letraCadena == "I" || letraCadena == "O" || letraCadena == "U" ) {
-                contadorTotalVocales ++;
+            if (letraCadena == 'A' || letraCadena == 'E' || letraCadena == 'I' || letraCadena == 'O'
+                    || letraCadena == 'U') {
+                contadorTotalVocales++;
             }
             return contadorTotalVocales;
         }
+    }
+
+    public static void main(String[] args) {
+        String resultadoContarVocales = contarVocalesEnCadena("hola");
+        System.out.println("Hay " + resultadoContarVocales + "vocales");
     }
 }
