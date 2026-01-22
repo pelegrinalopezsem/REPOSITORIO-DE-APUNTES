10bce9a8cd807c19358ac245b351988fee92b569
pelegrinalopezsem <pelegrinalopezsem001.alu@stemgranada.com>
2026-01-22 20:54:03 +0100

    eee

--- PROGRAMACION/UNIDAD_4/EJERCICIOS/UN2/Rel2/ejercicio7.java
+++ PROGRAMACION/UNIDAD_4/EJERCICIOS/UN2/Rel2/ejercicio7.java
@@ -1,7 +1,6 @@
 package PROGRAMACION.UNIDAD_4.EJERCICIOS.UN2.Rel2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ejercicio7 {
@@ -15,25 +14,44 @@ public class ejercicio7 {
         String nombre;
         int cantidad;
         int total;
+        int posMax;
+        int posMin;
         String mensaje = "1. Añadir gasto. \n2. Ver total. \n3. Ver máximo y mínimo. \n4. Promedio diario. \n5. Salir.";
-        //CREACION DE MENU
+        // CREACION DE MENU
         while (opcion != 5) {
             System.out.println(mensaje);
             opcion = scanner.nextInt();
-            if (opcion == 1){
+            if (opcion == 1) {
                 System.out.println("Nombre del gasto:");
                 nombre = scanner.next();
                 System.out.println("Cantidad:");
                 cantidad = scanner.nextInt();
                 nombreGastos.add(nombre);
                 cantidadGastos.add(cantidad);
-            } else if (opcion == 2){
+            } else if (opcion == 2) {
                 total = 0;
-                for (int i = 0; i < cantidadGastos.size(); i++){
+                for (int i = 0; i < cantidadGastos.size(); i++) {
                     total += cantidadGastos.get(i);
-                } 
+                }
                 System.out.println("Total: " + total + "€");
-            } 
+            } else if (opcion == 3) {
+                posMax = 0;
+                posMin = 0;
+                for (int i = 1; i < cantidadGastos.size(); i++) {
+                    if (cantidadGastos.get(i) > cantidadGastos.get(posMax))
+                        posMax = i;
+                    if (cantidadGastos.get(i) < cantidadGastos.get(posMin))
+                        posMin = i;
+                }
+                System.out.println("Mayor: " + nombreGastos.get(posMax) + " - " + cantidadGastos.get(posMax) + "\u20AC");
+                System.out.println("Menor: " + nombreGastos.get(posMin) + " - " + cantidadGastos.get(posMin) + "\u20AC");
+            } else if (opcion == 4) {
+                total = 0;
+                for (int i = 0; i < cantidadGastos.size(); i++)
+                    total += cantidadGastos.get(i);
+                System.out.println("Promedio diario: " + (total / 30));
+            }
         }
+        scanner.close();
     }
 }
---
+++ PROGRAMACION/UNIDAD_4/EJERCICIOS/UN2/Rel2/ejercicio8.java
@@ -0,0 +1,57 @@
+package PROGRAMACION.UNIDAD_4.EJERCICIOS.UN2.Rel2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // DECLARAMOS ARRAY
+        ArrayList<String> nombreReservas = new ArrayList<>(
+                Arrays.asList("Laura", "Victoria", "Pedro", "Alejandro"));
+        ArrayList<Integer> cantidadPersonas = new ArrayList<>(
+                Arrays.asList(2, 5, 3, 7));
+        // DECLARAMOS VARIABLES
+        int opcion = 0;
+        String mensaje = "1. Añadir reserva. \n2. Ver reservas. \n3. Total personas. \n4. Cancelar. \n5. Salir.";
+        String nombre;
+        int personas;
+        int total;
+        boolean encontrado = false;
+        // CREAMOS EL MENU
+        while (opcion != 5) {
+            System.out.println(mensaje);
+            opcion = scanner.nextInt();
+            if (opcion == 1) {
+                System.out.println("Nombre:");
+                nombre = scanner.next();
+                System.out.println("Número de personas:");
+                personas = scanner.nextInt();
+                nombreReservas.add(nombre);
+                cantidadPersonas.add(personas);
+            } else if (opcion == 2) {
+                System.out.println("Reservas: ");
+                for (int i = 0; i < nombreReservas.size(); i++) {
+                    System.out.println(nombreReservas.get(i) + " - " + cantidadPersonas.get(i));
+                }
+            } else if (opcion == 3) {
+                total = 0;
+                for (int i = 0; i < cantidadPersonas.size(); i++)
+                    total += cantidadPersonas.get(i);
+                System.out.println("Total personas: " + total);
+            } else if (opcion == 4) {
+                System.out.println("Nombre a cancelar:");
+                nombre = scanner.next();
+                for (int i = 0; i < nombreReservas.size() && !encontrado; i++) {
+                    if (nombreReservas.get(i).equals(nombre)) {
+                        encontrado = true;
+                        nombreReservas.remove(nombreReservas.size() - 1);
+                        cantidadPersonas.remove(cantidadPersonas.size() - 1);
+                    }
+                }
+            }
+        }
+        scanner.close();
+    }
+}
