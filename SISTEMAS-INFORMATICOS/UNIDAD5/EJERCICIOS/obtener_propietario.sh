79221c5a6061ded93e6d041d363dca5f7280aa6f
pelegrinalopezsem <pelegrinalopezsem001.alu@stemgranada.com>
2026-02-24 13:41:27 +0100

    otro nmas

--- SISTEMAS-INFORMATICOS/UNIDAD5/EJERCICIOS/obtener_propietario.sh
+++ SISTEMAS-INFORMATICOS/UNIDAD5/EJERCICIOS/obtener_propietario.sh
@@ -1,9 +1,12 @@
 obtener_propietario(){
-    if [ ! -s "$2" ]; then
-    echo "Formato no valido o vacio"
-    else
-    ls -l $1
-    echo "$1 : prop"
+    if [ ! -f "$1" ]; then
+        echo "Error: El archivo '$1' no existe o es un directorio."
+        return 1
     fi
 
+    local prop=$(stat -c '%U' "$1")
+    
+    echo "Archivo: $1 | Propietario: $prop"
 }
+
+obtener_propietario $1
